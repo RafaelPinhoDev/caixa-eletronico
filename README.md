953a9cd5b0c01aa946eb8bc7a701eff092810a8b
RafaelPinhoDev <rafael.pinhodev@gmail.com>
2025-03-29 19:57:46 -0300

    Correção do README

---
+++ README.md
@@ -0,0 +1,31 @@
+
+# Projeto Caixa Eletrônico
+
+Esse projeto é um caixa eletrônico que faz o saque da forma mais otimizada possível. Ele recebe o valor que você quer sacar e devolve a menor quantidade de cédulas possíveis, sempre usando as notas disponíveis: R$ 1, R$ 2, R$ 5, R$ 10, R$ 20, R$ 50 e R$ 100.
+
+O sistema calcula tudo automaticamente, começando pelas notas maiores e depois ajustando com as menores, garantindo que você receba o dinheiro de forma eficiente.
+
+__Como funciona:__
+
+    - Você digita o valor que quer sacar.
+    - O programa calcula e separa as notas da melhor forma.
+    - Ele te mostra exatamente quantas cédulas de cada valor você vai receber.
+
+
+
+
+
+## Sobre Mim
+
+Sou Rafael Souza de Pinho, tenho 20 anos, moro em São Roque-SP. Sou técnico em Desenvolvimento de Sistemas pela ETEC de São Roque e atualmente curso Sistemas para Internet na FATEC São Roque.
+
+[![Linktree](https://img.shields.io/badge/linktree-39E09B?style=for-the-badge&logo=linktree&logoColor=white)](https://linktree.com/rafaelsouzapinho)
+
+
+
+
+
+
+
+
+
---
+++ caixa-eletronico.py
@@ -0,0 +1,37 @@
+print("-------------------------------")
+print("CAIXA ELETRÔNICO")
+print("-------------------------------")
+print("Notas Disponíveis: R$ 1, R $2, R$ 5,R$ 10,R$ 20, R$ 50, R$ 100")
+print("                                                                ")
+saldo = int(input("Digite o valor que deseja sacar: R$"))
+print("                                                                ")
+n100 = n50 = n20 = n10 = n5 = n2 = n1 = 0
+
+if saldo >= 100:
+    n100 = saldo//100
+    saldo = saldo - n100*100
+if saldo >= 50:
+    n50 = saldo//50
+    saldo = saldo - n50*50
+if saldo >= 20:
+    n20 = saldo//20
+    saldo = saldo - n20*20
+if saldo >= 10:
+    n10 = saldo//10
+    saldo = saldo - n10*10
+if saldo >= 5:
+    n5 = saldo//5
+    saldo = saldo - n5*5
+if saldo >= 2:
+    n2 = saldo//2
+    saldo = saldo - n2*2
+if saldo >= 1:
+    n1 = saldo
+
+print("Notas de R$ 100: ", n100)
+print("Notas de R$ 50: ", n50)
+print("Notas de R$ 20: ", n20)
+print("Notas de R$ 10: ", n10)
+print("Notas de R$ 5: ", n5)
+print("Notas de R$ 2: ", n2)
+print("Notas de R$ 1: ", n1)
